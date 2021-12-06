85a8be986068c9b8727c653e872c17d4f1a388d0
SamiS7 <sami-abbas-ali@gmx.net>
2021-12-06 14:08:34 +0100

    commit design

--- src/main/java/com/htlleonding/watchlist/Main.java
+++ src/main/java/com/htlleonding/watchlist/Main.java
@@ -2,16 +2,43 @@ package com.htlleonding.watchlist;
 
 import com.htlleonding.watchlist.UI.pages.Home;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    Integer userId;
-    Integer width;
-    Integer height;
+    Integer userId = 0;
+    Integer width = 900;
+    Integer height = 600;
     @Override
     public void start(Stage stage) {
-        Home homePage = new Home(userId, width, height);
-        stage.setScene(homePage);
+        Home.setUserId(userId);
+        HBox root = new HBox();
+        Home homePage = new Home();
+
+        VBox menu = new VBox();
+        menu.getStyleClass().add("menu");
+        //Image menuIcon = new Image(Home.class.getResourceAsStream("/images/menuIcon.png"));
+        //ImageView menuIV = new ImageView(menuIcon);
+        Button menuIconB = new Button();
+        //menuIconB.setGraphic(menuIV);
+        menuIconB.getStyleClass().add("menuIcon");
+        Button myWatchlistB = new Button("Meine Merkliste");
+        Button seenB = new Button("Gesehen");
+        Button notSeenB = new Button("Nicht gesehen");
+        Button famousB = new Button("Beliebt");
+        Button searchB = new Button("Suchen");
+
+        menu.getChildren().addAll(menuIconB, myWatchlistB, seenB, notSeenB, famousB, searchB);
+
+        root.getStylesheets().add(getClass().getResource("/css/menu.css").toExternalForm());
+        root.getChildren().addAll(menu, homePage);
+        Scene scene = new Scene(root, width, height);
+        stage.setScene(scene);
         stage.setTitle("Watchlist");
         stage.show();
     }
--- src/main/java/com/htlleonding/watchlist/UI/pages/Home.java
+++ src/main/java/com/htlleonding/watchlist/UI/pages/Home.java
@@ -2,34 +2,26 @@ package com.htlleonding.watchlist.UI.pages;
 
 import com.htlleonding.watchlist.UI.components.MovieRow;
 import com.htlleonding.watchlist.enums.ListCategory;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class Home extends Scene {
+public class Home extends VBox {
     private static Integer userId;
     private static int rowLimit = 5;
 
-    public Home(Integer userId) {
-        super(initRoot());
-        this.userId = userId;
-
-    }
-    public Home(Integer userId, int w, int h) {
-        super(initRoot(), w, h);
-        this.userId = userId;
-
-    }
-
-    public static Parent initRoot() {
-        HBox root = new HBox();
-        VBox menu = new VBox();
+    public Home() {
         VBox content = new VBox();
-        root.getChildren().addAll(menu, content);
+        this.getChildren().add(content);
 
+        content.getStyleClass().add("content");
         content.getChildren().addAll(new MovieRow(ListCategory.SHORTLY_SAVED, userId, rowLimit));
+    }
 
-        return root;
+    public static Integer getUserId() {
+        return userId;
+    }
+
+    public static void setUserId(Integer userId) {
+        Home.userId = userId;
     }
 }
